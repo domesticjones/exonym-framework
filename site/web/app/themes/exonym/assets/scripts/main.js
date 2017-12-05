1b0bce3cc61476e0892a423dbf149c20a589b196
Dustin Jones <info@domesticjones.com>
2017-12-04 21:51:57 -0700

    Implement parallax options for modules

--- site/web/app/themes/exonym/assets/scripts/main.js
+++ site/web/app/themes/exonym/assets/scripts/main.js
@@ -33,47 +33,32 @@ jQuery(document).ready(() => {
     fade: true,
   });
 
-
-
-
-
-
-  // MODAL: Functions
-  $('.module-expand').click(e => {
-    e.preventDefault();
-    window.location.hash = '';
-    $('#modal').fadeIn();
-    const $content = $('#modal-content');
-    const type = $(e.currentTarget).data('expand');
-    if(type === 'image') {
-      const image = $(e.currentTarget).data('content');
-      $content.addClass('modal-image');
-      $content.html(`<img src="${image}" />`);
-    } else if(type === 'video') {
-      $content.addClass('modal-video');
-      const videoUrl = $(e.currentTarget).parent().find('.video-container iframe').attr('src');
-      const video = $(e.currentTarget).parent().find('.video-container').clone();
-      $content.html(video);
-      $content.find('.video-container iframe').attr('src', `${videoUrl}&autoplay=1`);
-    } else if(type === 'text') {
-      $content.addClass('modal-text');
-      const text = $(e.currentTarget).parent().find('.modal-text-hidden').html();
-      $content.html(text);
-    }
+  // MODULES: Parallax
+  $(window).on('load resize scroll', () => {
+    const d_scroll = $(window).scrollTop();
+    const w_height = $(window).height();
+    $('.animate-parallax').each((i,e) => {
+      const $this = $(e);
+      const $thisBg = $this.find('.module-bg');
+      const p_position = $this.offset().top;
+      const e_height = $this.outerHeight();
+      const ebg_height = $this.find('.module-bg').outerHeight();
+      const bg_diff = ebg_height - e_height;
+      const dhit_in = p_position - w_height;
+      const dhit_out = p_position + e_height;
+      const dhit_read = p_position - w_height - d_scroll;
+      // Boolean hit Check
+      if (dhit_read <= 0 && d_scroll <= dhit_out) {
+        const per_scrolled = (d_scroll - dhit_in) / (dhit_out - dhit_in);
+        const offset = (bg_diff * per_scrolled) ;
+        $thisBg.css('transform', `translateY(-${offset}px)`);
+      }
+    });
   });
 
-  // MODAL: Close
-  $('#modal-close').click(() => {
-    $('#modal-content').removeClass('modal-image modal-video modal-text').empty();
-    $('#modal').fadeOut();
-  });
 
-  // VIDEO: Play on thumbnail click
-  $('.module-video-thumb').click((e) => {
-    $(e.currentTarget).fadeOut();
-    const videoUrl = $(e.currentTarget).parent().find('.video-container iframe').attr('src');
-    $(e.currentTarget).parent().find('.video-container iframe').attr('src', `${videoUrl}&autoplay=1`);
-  });
+
+
 
   // MODULES: Animate onScreen
   $(window).on('load resize scroll', () => {
